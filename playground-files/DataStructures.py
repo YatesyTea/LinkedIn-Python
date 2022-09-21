3406afa0532d2f6ca0005effd8ddd2b5d240ce8c
YatesyTea <39750168+YatesyTea@users.noreply.github.com>
2022-09-21 18:35:03 +0100

    added playground files for messing with diff parts

---
+++ playground-files/DataStructures.py
@@ -0,0 +1,10 @@
+a = [1,2,3]
+print(a)
+
+# List Slicing
+b = [1,2,3,4,5]
+c = b[:2] # This takes the first 2 elements.
+print(c) 
+
+d = b[0:6:2] # In this case we have [Start:End:Spacing]
+print(d)
