1527080ab1c8ea12ba7e5f8fd5447a1ddf9d800a
YatesyTea <39750168+YatesyTea@users.noreply.github.com>
2022-09-22 18:35:23 +0100

    Cleared up all of the files that linkedin supplies so it's only my own.

--- Exercise-Files/python-essential-training-2449125-main/exercise_files/01_06_solution.py
+++
@@ -1,8 +0,0 @@
-from termcolor import colored
-
-print(colored('~~~RAINBOW~~~', 'red'))
-print(colored('~~~RAINBOW~~~', 'yellow'))
-print(colored('~~~RAINBOW~~~', 'green'))
-print(colored('~~~RAINBOW~~~', 'cyan'))
-print(colored('~~~RAINBOW~~~', 'blue'))
-print(colored('~~~RAINBOW~~~', 'magenta'))
