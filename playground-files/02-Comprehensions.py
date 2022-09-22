9a3eeea823a803383994beb317b27010359d0b9c
YatesyTea <39750168+YatesyTea@users.noreply.github.com>
2022-09-22 18:23:04 +0100

    completed research and explanations on comprehensions, more experimentation with nested required

--- playground-files/02-Comprehensions.py
+++ playground-files/02-Comprehensions.py
@@ -5,9 +5,33 @@ def genList(x):
 
 # List Comprehensions
 
-# Simple function
+# Simple Operations on Lists
+print('######### Simple Operations ########## \n\n\n')
 a = genList(10)
 a2 = [2* item for item in a]
-print(a2)
+print(f'Converting from {a} --> {a2} \n\n\n')
 
+b = genList(100)
+print('[item for item in list if item % 10 == 0] \n Does this:')
+b2 = [item for item in b if item % 10 == 0]
+print(f'Converting from list of 0 to 100 --> {b2} using modulo and if statement.')
 
+# Calling Functions
+print('#### List Comprehensions With Functions ####')
+# Strings are treated as lists in Python in most cases.
+myString = "My name is Jimbob. I have no control over my baking skills. They fluctuate"
+
+def cleanWord(word):
+    return word.replace(',','').lower() # This replaces every instance of a , with empty string, and then sets it to lowercase.
+print(myString)
+print('Goes to')
+cleaned = [cleanWord(word) for word in myString.split() if len (cleanWord(word)) < 4]
+print(cleaned)
+print("The generic form is [function for element in list if statement]")
+
+# Nested List Comprehensions
+separatedSentence = [sentence for sentence in myString.split(',')]
+cleaned = [cleanWord(word) for word in myString.split()]
+# Both of these above can be nested together into a one liner SO PYTHONIC!
+fullyDone = [[cleanWord(word) for word in sentence.split()] for sentence in myString.split('.')]
+print(fullyDone)
